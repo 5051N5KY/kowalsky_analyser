a519181dfa563f8b68e52bb611e0623bf3f366ae
Sosinsky <przemyslaw.sosinski@hotmail.com>
2024-07-16 21:12:28 +0200

    Added home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use League\Flysystem\FilesystemOperator;
 
 class HomeController extends AbstractController
 {
+    public function __construct(private FilesystemOperator $defaultStorage)
+    {
+
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $this->defaultStorage->write('default.txt', 'Test');
+        //dd($this->defaultStorage->listContents('', 100)->toArray());
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'controller_name' => $this->defaultStorage->read('default.txt'),
         ]);
     }
 }
