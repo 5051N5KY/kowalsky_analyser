ac6fe29a74879919294925ee1677d6b0d5f40ba7
Sosinsky <przemyslaw.sosinski@hotmail.com>
2024-07-24 01:09:02 +0200

    Usuwanie plików i hover na obrazki

--- public/js/home.js
+++ public/js/home.js
@@ -9,3 +9,34 @@ function closeModal() {
     var modal = document.getElementById("modal");
     modal.style.display = "none";
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Dodanie funkcji usuwania plików
+    window.deleteFile = function (filePath) {
+        if (confirm('Czy na pewno chcesz usunąć ten obiekt?')) {
+            console.log(filePath);
+            fetch(deleteFileUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: new URLSearchParams({
+                    'path': filePath
+                })
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.status === 'success') {
+                        alert('Obiekt usunięty!');
+                        location.reload(); // Reload the page to reflect changes
+                    } else {
+                        alert('Error deleting file: ' + data.message);
+                    }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    alert('An error occurred while deleting the file');
+                });
+        }
+    }
+});
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,12 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use League\Flysystem\FilesystemOperator;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class HomeController extends AbstractController
 {
+    const STORAGE_PREFIX = 'storage/';
+
     public function __construct(private FilesystemOperator $mainStorage)
     {
     }
@@ -17,19 +21,35 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         $objects = $this->getPhotos();
-   
         return $this->render('home/index.html.twig', ['items' => $objects]);
     }
 
-    public function getPhotos(): array
+    #[Route('/deleteFile', name: 'delete_file', methods: ['POST'])]
+    public function deleteFile(Request $request): JsonResponse
+    {
+        $filePath = $request->request->get('path');
+        $filePathWithoutPrefix = str_replace(self::STORAGE_PREFIX, '', $filePath);
+
+        try {
+            if ($this->mainStorage->fileExists($filePathWithoutPrefix)) {
+                $this->mainStorage->delete($filePathWithoutPrefix);
+                return new JsonResponse(['status' => 'success'], Response::HTTP_OK);
+            }
+        } catch (\Exception $e) {
+            return new JsonResponse(['status' => 'error', 'message' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return new JsonResponse(['status' => 'error', 'message' => 'File not found'], Response::HTTP_NOT_FOUND);
+    }
+
+    private function getPhotos(): array
     {
         $files = $this->mainStorage->listContents('objects/', 'files')->toArray();
         $paths = [];
-        $storagePath = 'storage/';
+
         foreach ($files as $file) {
-            $paths[] = $storagePath.$file['path'];
+            $paths[] = self::STORAGE_PREFIX . $file['path'];
         }
-        
         return $paths;
     }
 }
